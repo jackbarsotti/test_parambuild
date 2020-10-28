ce78b4d8f948e78ac767a466aacfc67f60a6453b
Jack Barsotti <jackbarsotti1@gmail.com>
2020-10-28 14:23:19 -0500

    update

---
+++ scripts/bash/destructiveChange.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# inputs:
+#deploydir updated with subdirectory "deployment", which is our new deployment package(s) parent dir:
+export DEPLOYDIR=/var/lib/jenkins/workspace/parambuild_${deployBranchURL}/bitbucket-checkout/force-app/main/default/deployment
+#create destructivePackage folder (incremental build creates incrementalPackage folder within same deploydir):
+export destructivePackagePath=$DEPLOYDIR/destructivePackage
+#diff files will be moved into the classes folder:
+export SOURCE_PATH=$destructivePackagePath/classes
+export API_VERSION=49.0 # api version (same as in the sfdx-project.json)
+
+# Copy D files from git diff to destructive changes folder
+  #originally: git log --oneline --diff-filter=D --summary
+  #using git diff-tree instead because the output is much shorter (only file names) and therefore easier to use:
+git diff-tree --no-commit-id --name-only -r HEAD --diff-filter=D |
+  while read -r file; do
+    sudo cp "$file" $SOURCE_PATH 2>/dev/null
+  done
+
+echo "Converting Source format to Metadata API format"
+#convert existing git diff files in the classes folder into metadata api format
+sfdx force:source:convert -p ${SOURCE_PATH} -d ${SOURCE_PATH}
+
+# copy package.xml to destructiveChangesPre.xml
+cp manifest/package.xml $destructivePackagePath/destructiveChangesPre.xml
+
+# generate an empty (containing only the api version tag) package.xml (why?)
+cat <<EOT > $destructivePackagePath/package.xml
+<?xml version="1.0" encoding="UTF-8"?>
+<Package xmlns="http://soap.sforce.com/2006/04/metadata">
+  <version>${API_VERSION}</version>
+</Package>
+EOT
---
+++ scripts/bash/incrementalBuild.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#deploydir updated with subdirectory "deployment", which is our new deployment package(s) parent dir:
+export DEPLOYDIR=/var/lib/jenkins/workspace/parambuild_${deployBranchURL}/bitbucket-checkout/force-app/main/default/deployment
+#create incrementalPackage folder within deploydir
+export incrementalPackagePath=$DEPLOYDIR/incrementalPackage
+#diff files will be moved into the classes folder:
+export SOURCE_PATH=$incrementalPackagePath/classes
+export classPath=/var/lib/jenkins/workspace/parambuild_${deployBranchURL}/bitbucket-checkout/force-app/main/default/classes
+export triggerPath=/var/lib/jenkins/workspace/parambuild_${deployBranchURL}/bitbucket-checkout/force-app/main/default/triggers
+
+# Git Diff Section:
+
+#using git diff-tree instead because the output is much shorter (only file names) and therefore easier to use:
+git diff-tree --no-commit-id --diff-filter=UMA --name-only -r HEAD | 
+while read -r file; do
+  # Copy the files from git diff into the deploy directory:
+  sudo cp --parents "$file" $SOURCE_PATH 2>/dev/null
+  # For any changed class, trigger, page file, it's associated meta data file is copied to the deploy directory (and vice versa):
+  if [[ $file == *.cls ]]; then
+    find $classPath -samefile "$file-meta.xml" -exec sudo cp --parents -t $SOURCE_PATH {} \;
+  elif [[ $file == *.cls-meta.xml ]]; then
+    parsedfile=${file%.cls-meta.xml}
+    find $classPath -samefile "$parsedfile.cls" -exec sudo cp --parents -t $SOURCE_PATH {} \;
+  elif [[ $file == *.trigger ]]; then
+    find $triggerPath -samefile "$file-meta.xml" -exec sudo cp --parents -t $SOURCE_PATH {} \;
+  elif [[ $file == *.trigger-meta.xml ]]; then
+    parsedfile=${file%.trigger-meta.xml}
+    find $triggerPath -samefile "$parsedfile.trigger" -exec sudo cp --parents -t $SOURCE_PATH {} \;
+  fi
+done 
+
+echo "Converting Source format to Metadata API format"
+sfdx force:source:convert -p ${SOURCE_PATH} -d ${SOURCE_PATH}
+
+#Need to generate package xml here with the above files
